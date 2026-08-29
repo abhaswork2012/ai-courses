9f6f37506e535fc6bb7f29597f65df20f9cd0b87
Abhas Bhatia <abhas.work2012@gmail.com>
2026-08-29 16:57:43 +0530

    Remove stale files before reading tree

--- python_git/python_git/base.py
+++ python_git/python_git/base.py
@@ -39,7 +39,7 @@ def get_tree (oid, base_path=''):
     for type_, oid, name in _iter_tree_entries (oid):
         assert '/' not in name
         assert name not in ('..', '.')
-        path = base_path + name
+        path = base_path name
         if type_ == 'blob':
             result[path] = oid
         elif type_ == 'tree':
@@ -48,8 +48,26 @@ def get_tree (oid, base_path=''):
             assert False, f'Unknown tree entry {type_}'
     return result
 
+def _empty_current_directory ():
+    for root, dirnames, filenames in os.walk ('.', topdown=False):
+        for filename in filenames:
+            path = os.path.relpath (f'{root}/{filename}')
+            if is_ignored (path) or not os.path.isfile (path):
+                continue
+            os.remove (path)
+        for dirname in dirnames:
+            path = os.path.relpath (f'{root}/{dirname}')
+            if is_ignored (path):
+                continue
+            try:
+                os.rmdir (path)
+            except (FileNotFoundError, OSError):
+                # Deletion might fail if the directory contains ignored files,
+                # so it's OK
+                pass
 
 def read_tree (tree_oid):
+    _empty_current_directory ()
     for path, oid in get_tree (tree_oid, base_path='./').items ():
         os.makedirs (os.path.dirname (path), exist_ok=True)
         with open (path, 'wb') as f:
