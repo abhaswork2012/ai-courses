ec23b3ecf94baea2db901fad241f2591d7da8741
Abhas Bhatia <abhas.work2012@gmail.com>
2026-08-29 17:55:42 +0530

    adding default HEAD commit to pass

--- python_git/python_git/base.py
+++ python_git/python_git/base.py
@@ -123,6 +123,7 @@ def get_commit (oid):
     return Commit (tree=tree, parent=parent, message=message)
 
 def get_oid (name):
+    if name == '@': name = 'HEAD'
     # Name is ref
     refs_to_try = [
         f'{name}',
