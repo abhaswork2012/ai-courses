b58f8d7ed42b8feabfa9f74b341166c7bbcd5467
Abhas Bhatia <abhas.work2012@gmail.com>
2026-08-29 16:51:07 +0530

    Adding write_tree function for listing directories

--- python_git/python_git/cli.py
+++ python_git/python_git/cli.py
@@ -40,3 +40,6 @@ def hash_object (args):
 def cat_file (args):
     sys.stdout.flush ()
     sys.stdout.buffer.write (data.get_object (args.object, expected=None))
+
+def write_tree(args):
+    print (base.write_tree ())
