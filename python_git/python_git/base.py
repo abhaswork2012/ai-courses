1311e0c14cf544b67172d7852ee24e986957d64a
Abhas Bhatia <abhas.work2012@gmail.com>
2026-08-29 17:23:48 +0530

    adding basic git tag functionality

--- python_git/python_git/base.py
+++ python_git/python_git/base.py
@@ -79,8 +79,7 @@ def read_tree (tree_oid):
 
 def commit (message):
     commit = f'tree {write_tree ()}\n'
-
-    HEAD = data.get_HEAD ()
+    HEAD = data.get_ref ('HEAD')
     if HEAD:
         commit += f'parent {HEAD}\n'
 
@@ -91,17 +90,19 @@ def commit (message):
 
     oid = data.hash_object (commit.encode (), 'commit')
 
-    data.set_HEAD (oid)
+    data.update_ref ('HEAD', oid)
 
     return oid
 
 def checkout (oid):
     commit = get_commit (oid)
     read_tree (commit.tree)
-    data.set_HEAD (oid)
+    data.update_ref ('HEAD', oid)
 
 Commit = namedtuple ('Commit', ['tree', 'parent', 'message'])
 
+def create_tag (name, oid):
+    data.update_ref (f'refs/tags/{name}', oid)
 
 def get_commit (oid):
     parent = None
